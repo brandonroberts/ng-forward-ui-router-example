d6d0952ba592fdc01be37be0558bc8183d18ed19
Brandon Roberts <robertsbt@gmail.com>
2015-11-30 20:11:16 -0600

    Added example otherwise route

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -2,12 +2,13 @@ import {Component, StateConfig} from 'ng-forward';
 import {Home} from '../home/home';
 import {About} from '../about/about';
 import {Users} from '../users/users';
+import {NotFound} from '../errors/notfound';
 
 @Component({
   selector: 'app',
   template: `
     Test App
-    <a ui-sref="home">Home</a> | <a ui-sref="about">About</a> | <a ui-sref="users.profile({id: 123 })">Profile</a>
+    <a ui-sref="home">Home</a> | <a ui-sref="about">About</a> | <a ui-sref="users.profile({id: 123 })">Profile</a> | <a href="/invalid">Invalid Page</a>
     <hr>
     <div ui-view></div>
     `,
@@ -16,7 +17,8 @@ import {Users} from '../users/users';
 @StateConfig([
   { url: '/', component: Home, name: 'home' },
   { url: '/about', component: About, name: 'about' },
-  { url: '/users/:id', component: Users, name: 'users' }
+  { url: '/users/:id', component: Users, name: 'users' },
+  { url: '/404', component: NotFound, name: 'notfound' }
 ])
 export class App {
 
--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,6 @@
 export default angular.module('app.config', [])
-.config(['$locationProvider', ($locationProvider) => {
+.config(['$locationProvider', '$urlRouterProvider', ($locationProvider, $urlRouterProvider) => {
   $locationProvider.html5Mode({ enabled: true, requireBase: false });
+
+  $urlRouterProvider.otherwise('/404');
 }]);
